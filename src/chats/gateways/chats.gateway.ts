a4a1ca643652cf91de7494ed5ebe6bc86633bbb3
khalid_shaikh <khalid.shaik@oryx.edu.qa>
2024-10-01 17:56:33 +0300

    add chat service code

--- src/chats/gateways/chats.gateway.ts
+++ src/chats/gateways/chats.gateway.ts
@@ -1,18 +1,55 @@
 import {
+  // ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { ChatsService } from '../services/chats.service';
 
 @WebSocketGateway({ cors: true })
-export class ChatsGateway {
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
   constructor(private chatsService: ChatsService) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  afterInit(server: Server) {
+    console.log('server initialized');
+  }
+
+  handleConnection(client: Socket) {
+    console.log('client connected', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('client disconnected', client.id);
+  }
+
   @SubscribeMessage('chat-message')
-  async handleMessage(@MessageBody() payload: string) {
+  async handleMessage(
+    @MessageBody()
+    payload: {
+      message: string;
+      senderId: string;
+      recieverId: string;
+    },
+  ) {
     console.log(payload);
-    const response = await this.chatsService.saveOneToOneChat(payload);
-    return response;
+    const { message, senderId, recieverId } = payload;
+    const saved = await this.chatsService.saveOneToOneChat({
+      senderId,
+      recieverId,
+      message,
+    });
+    this.server.to(`user_${recieverId}`).emit('chat-message', saved);
+    this.server.to(`user_${senderId}`).emit('chat-message', saved);
   }
 }
--- src/chats/services/chats.service.ts
+++ src/chats/services/chats.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Message } from '../entities/messages.entity';
 import { DataSource, Repository } from 'typeorm';
+import { User } from 'src/users/entities/users.entity';
+import { UserContact } from 'src/users/entities/user-contact.entity';
+import { Conversation } from '../entities/conversation.entity';
+import { MessageReceipt } from '../entities/message-receipt.entity';
 
 @Injectable()
 export class ChatsService {
@@ -10,7 +14,76 @@ export class ChatsService {
     private dataSource: DataSource,
   ) {}
 
-  async saveOneToOneChat(payload: any) {
-    console.log(payload);
+  async canCommunicate(senderId: string, recieverId: string): Promise<boolean> {
+    const contact = await this.dataSource.manager.findOne(UserContact, {
+      where: [
+        {
+          user: { id: senderId },
+          contact: { id: recieverId },
+          status: 'accepted',
+        },
+      ],
+    });
+
+    return !!contact;
+  }
+
+  async saveOneToOneChat(payload: {
+    senderId: string;
+    recieverId: string;
+    message: string;
+  }) {
+    const { senderId, recieverId, message } = payload;
+    const canCommunicate = await this.canCommunicate(senderId, recieverId);
+    if (!canCommunicate) {
+      throw new ForbiddenException(
+        'You are not allowed to send messages to this user.',
+      );
+    }
+
+    let conversation = await this.dataSource.manager.findOne(Conversation, {
+      where: [
+        {
+          participants: [
+            { user: { id: senderId } },
+            { user: { id: recieverId } },
+          ],
+        },
+        {
+          participants: [
+            { user: { id: recieverId } },
+            { user: { id: senderId } },
+          ],
+        },
+      ],
+      relations: ['participants'],
+    });
+
+    if (!conversation) {
+      conversation = this.dataSource.manager.create(Conversation, {
+        type: 'private',
+        participants: [
+          { user: { id: senderId } },
+          { user: { id: recieverId } },
+        ],
+      });
+      await this.dataSource.manager.save(conversation);
+    }
+
+    const createdMessage = this.dataSource.manager.create(Message, {
+      conversation,
+      sender: { id: senderId } as User,
+      content: message,
+    });
+    const savedMessage = await this.dataSource.manager.save(createdMessage);
+
+    const receipt = this.dataSource.manager.create(MessageReceipt, {
+      message: savedMessage,
+      recipient: { id: recieverId } as User,
+      status: 'delivered',
+    });
+    await this.dataSource.manager.save(receipt);
+
+    return savedMessage;
   }
 }
